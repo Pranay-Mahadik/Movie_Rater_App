a2cf80311b5298abca014ffff964b12659e9fef7
Pranay Mahadik <pranaymhd98@gmail.com>
2022-12-23 10:23:44 +0530

    Initial Release

---
+++ movie-rater/movie_rater/src/index.js
@@ -0,0 +1,37 @@
+import React  from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import Auth from './components/auth';
+import reportWebVitals from './reportWebVitals';
+import { Route, BrowserRouter, Routes } from 'react-router-dom'
+import {CookiesProvider} from 'react-cookie';
+
+
+function Router() {
+  
+  return (
+
+    <React.StrictMode>
+      <CookiesProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route  path="/" element={<Auth />} />
+            <Route path="/movies" element={<App />} />
+          </Routes>
+        </BrowserRouter>
+      </CookiesProvider>
+    </React.StrictMode>
+
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Router />
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
